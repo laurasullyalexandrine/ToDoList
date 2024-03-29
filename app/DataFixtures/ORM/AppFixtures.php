5c77be2be1669dc0a73ced8bb03d4b71bbeafa9c
laurasullyalexandrine <launyie@gmail.com>
2024-03-29 14:27:21 +0100

    updating fixtures

--- app/DataFixtures/ORM/AppFixtures.php
+++ app/DataFixtures/ORM/AppFixtures.php
@@ -2,16 +2,37 @@
 
 namespace App\DataFixtures\ORM;
 
+use AppBundle\Entity\Task;
+use AppBundle\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
 
     public function load(ObjectManager $manager)
     {
-        var_dump('coucou');die;
-      
+        // Comparer les fixtures avec Todo v2
+        for ($u = 0; $u < 22; $u++) {
+            $user = new User();
+            $user->setUsername('usernumero' . $u);
+            $user->setEmail($user->getUsername() . '@todolist.fr');
+            $user->setPassword('$2y$13$ePq5oyaxgxQDsxbFaIB2/eBAiPHTaZ2eFZ3HqnYcwPDVbhntX1zR.');
+
+            $manager->persist($user);
+        }
+
+        for ($t = 0; $t < 463; $t++) {
+            $task = new Task();
+            $task->setTitle('Titre de la tâche n° ' . $t);
+            $task->setContent('Contenu de la tâche n° ' . $t);
+            $task->isDone(false);
+            $task->setCreatedAt(new \DateTime());
+
+            $manager->persist($task);
+        }
+
+        $manager->flush();
     }
 }
